f60d4d931150d29762b2b2721d24041f528f458a
jominmathewdeblase <jominm09@gmail.com>
2024-10-14 21:23:22 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,45 +50,45 @@ Questions:{input}
 
 """
 )
-
-def vector_embedding():
-
-    if "vectors" not in st.session_state:
-
-        st.session_state.embeddings=GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-        st.session_state.loader=PyPDFDirectoryLoader("./debase_doc_c") ## Data Ingestion
-        st.session_state.docs=st.session_state.loader.load() ## Document Loading
-        st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200) ## Chunk Creation
-        st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:20]) #splitting
-        st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings) #vector OpenAI embeddings
-
-
-
-
-
-prompt1=st.text_input("Enter Your Question From Documents")
-
-
-if st.button("Documents Embedding"):
-    vector_embedding()
-    st.write("Vector Store DB Is Ready")
-
-import time
-
-
-
-if prompt1:
-    document_chain=create_stuff_documents_chain(llm,prompt)
-    retriever=st.session_state.vectors.as_retriever()
-    retrieval_chain=create_retrieval_chain(retriever,document_chain)
-    start=time.process_time()
-    response=retrieval_chain.invoke({'input':prompt1})
-    print("Response time :",time.process_time()-start)
-    st.write(response['answer'])
-
-    # With a streamlit expander
-    with st.expander("Document Similarity Search"):
-        # Find the relevant chunks
-        for i, doc in enumerate(response["context"]):
-            st.write(doc.page_content)
-            st.write("--------------------------------")
+    
+    def vector_embedding():
+    
+        if "vectors" not in st.session_state:
+    
+            st.session_state.embeddings=GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+            st.session_state.loader=PyPDFDirectoryLoader("./debase_doc_c") ## Data Ingestion
+            st.session_state.docs=st.session_state.loader.load() ## Document Loading
+            st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200) ## Chunk Creation
+            st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:20]) #splitting
+            st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings) #vector OpenAI embeddings
+    
+    
+    
+    
+    
+    prompt1=st.text_input("Enter Your Question From Documents")
+    
+    
+    if st.button("Documents Embedding"):
+        vector_embedding()
+        st.write("Vector Store DB Is Ready")
+    
+    import time
+    
+    
+    
+    if prompt1:
+        document_chain=create_stuff_documents_chain(llm,prompt)
+        retriever=st.session_state.vectors.as_retriever()
+        retrieval_chain=create_retrieval_chain(retriever,document_chain)
+        start=time.process_time()
+        response=retrieval_chain.invoke({'input':prompt1})
+        print("Response time :",time.process_time()-start)
+        st.write(response['answer'])
+    
+        # With a streamlit expander
+        with st.expander("Document Similarity Search"):
+            # Find the relevant chunks
+            for i, doc in enumerate(response["context"]):
+                st.write(doc.page_content)
+                st.write("--------------------------------")
